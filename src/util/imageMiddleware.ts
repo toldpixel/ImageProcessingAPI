a22914fd40c850dba7b1d48ac2f15e41e157617d
xorunknown <software.nutshell@gmail.com>
2023-09-06 19:36:33 +0200

    check File exists function

--- src/util/imageMiddleware.ts
+++ src/util/imageMiddleware.ts
@@ -7,41 +7,90 @@ const sharp = require("sharp");
 // /api/images?filename=argentina&width=200&height=200
 interface CustomRequest extends Request {
     data?: Buffer;
+    filename?: string;
+    width?: string;
+    height?: string;
+    filepath?: string;
 }
+
+async function fileExists(filename: string): Promise<boolean> {
+    let check: boolean = true;
+    try {
+        await fs.access(`public/assets/img/thumbs/${filename}_thumbs.jpg`, fs.constants.F_OK);
+    } catch (err: NodeJS.ErrnoException | unknown ) {
+        if(err && (err as NodeJS.ErrnoException).code === 'ENOENT') {
+            console.error('File does not exist');
+            check = false;
+            return check;
+        } else {
+            console.error('Error in File Check:', err);
+        }
+    }
+    return check;
+}
+
 
 const middleware = {
 
-    async readImage(req: CustomRequest, res: Response, next: NextFunction): Promise<void> { 
+    async readParams(req: CustomRequest, res: Response, next: NextFunction): Promise<boolean> { 
         try {
-            //console.log(path.join(__dirname, '../../public/assets/img/full', req.query.filename + '.jpg'));
-            //req.data = await fs.readFile(path.join(__dirname, '../../public/assets/img/full', req.query.filename + '.jpg'));
-            req.data = path.join(__dirname, '../../public/assets/img/full', req.query.filename + '.jpg');        
+
+            const filename = req.query.filename as string | undefined;
+            const filepath = path.join(__dirname, '../../public/assets/img/full', req.query.filename as string | undefined + '.jpg');
+            const width = req.query.width as string | undefined;
+            const height = req.query.height as string | undefined;
+            const defaultWidth = '200';
+            const defaultHeight = '200';
+            
+            if(!filename) {
+               throw new Error('No filename given'); 
+            }
+
+            req.filename = filename;
+            req.filepath = filepath as string;
+            req.width = width || defaultWidth; 
+            req.height = height || defaultHeight;
+
+            if(await fileExists(filename)) {
+                console.log("File exists!");
+                return true;
+            }
+  
+            next();
         } catch (err) {
-            console.log(err);
+            console.log("Error in readParams" + err);
             res.writeHead(404, { 'Content-Type': 'text/plain'});
             res.end('File not found');
         }
-        next();
+        return false;
     },
 
     async resizeImage(req: CustomRequest, res: Response, next: NextFunction) {
         try {
-           const fileName = req.data
-           const fileWidth = parseInt(req.query.width as string);
-           const fileHeight = parseInt(req.query.height as string);
-           console.log(req.data, fileWidth, fileHeight); 
-           await sharp(fileName)
+           console.log(req.filename, req.width, req.height, req.filepath); 
+           await sharp(req.filepath)
                 .resize({
-                width: fileWidth,
-                height: fileHeight
+                width: parseInt(req.width as string),
+                height: parseInt(req.height as string)
                 })
-                .toFile(`${fileName}_thumbs.jpg`);
-                res.setHeader('Content-Type', 'image/jpeg');
-                res.end(req.data);
+                .toFile(`public/assets/img/thumbs/${req.filename}_thumbs.jpg`);
         } catch (error) {
-            console.log(error);
+            console.log("Error in resize Image:" + error);
         }
         next();
+    },
+
+    async sendImage(req: CustomRequest, res: Response, next: NextFunction): Promise<void> { 
+        try {
+            console.log("read file from harddrive");
+            req.data = await fs.readFile(path.join(__dirname, '../../public/assets/img/thumbs', req.filename + '_thumbs.jpg'));
+            res.setHeader('Content-Type', 'image/jpeg');
+            res.end(req.data);
+        } catch (err) {
+            console.log("Error in sendImage:" + err);
+            res.writeHead(404, { 'Content-Type': 'text/plain'});
+            res.end('File not found');
+        }
     },
 
 }
